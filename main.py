2088454665ed3177416f0986e8dbad955865f0f9
Jo JangHyen(home) <pa7333@gmail.com>
2025-10-14 21:58:03 +0900

    게임 객체 관리를 위한 world 변수 추가 완료

--- main.py
+++ main.py
@@ -13,11 +13,30 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+def reset_world():
+    global world
+    world = []
 
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
 
 running = True
 open_canvas()
+reset_world()
 while running:
     handle_events()
+    update_world()
+    render_world()
     delay(0.01)
 close_canvas()
