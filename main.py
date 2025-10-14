f444efa94077612ca061bf85f4b94ac39b573a31
Jo JangHyen(home) <pa7333@gmail.com>
2025-10-14 22:31:54 +0900

    궁수 Archer IDLE 애니메이션 출력 완료

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from pico2d import *
 
 
+characterjob = 'Archer'
+
 class Character:
     image = None
+
     def __init__(self):
         self.x, self.y = 400, 400
         self.frame = 0
-        if Character.image is None:
+        if Character.image is None and characterjob == 'Swordsman':
             Character.image = load_image('Swordsman/Idle.png')
+        elif Character.image is None and characterjob == 'Archer':
+            Character.image = load_image('Archer/Idle.png')
+
         pass
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        if characterjob == 'Swordsman':
+            self.frame = (self.frame + 1) % 8
+        elif characterjob == 'Archer':
+            self.frame = (self.frame + 1) % 6
+
         pass
 
     def draw(self):
@@ -18,15 +28,17 @@ class Character:
         pass
 
 
+
 def handle_events():
     global running
-
     event_list = get_events()
     for event in event_list:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
+
 
 def reset_world():
     global world
@@ -51,7 +63,9 @@ def render_world():
     update_canvas()
 
 running = True
+
 open_canvas()
+
 reset_world()
 while running:
     handle_events()
