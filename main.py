b707eff6523d9477b2dcb809cb56ace4c012f934
Jo JangHyen(home) <pa7333@gmail.com>
2025-10-14 21:55:48 +0900

    초기 설정 완료

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from pico2d import *
 
+
+
+
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+
+running = True
 open_canvas()
-
-
+while running:
+    handle_events()
+    delay(0.01)
 close_canvas()
