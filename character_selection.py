bcaea9bde850653af33197dc56ed43847212f311
Jo JangHyen(home) <pa7333@gmail.com>
2025-11-10 22:24:49 +0900

    game_framework를 활용하여 화면별 파일 분리 및 상태 관리 도입

---
+++ character_selection.py
@@ -0,0 +1,66 @@
+import game_framework
+from pico2d import *
+import play_mode
+from Character import Character
+
+CHARACTER_POSITIONS = {
+    # Swordsman 영역 (예시: x=100 ~ 300)
+    'Swordsman': (50, 300),
+    # Wizard 영역 (예시: x=350 ~ 550)
+    'Wizard': (300, 550),
+    # Archer 영역 (예시: x=600 ~ 800)
+    'Archer': (550, 800)
+}
+# 캐릭터들의 Y축 범위는 캔버스 중앙 근처 (851px 기준, 예를 들어 y=400 ~ 500)
+CHARACTER_Y_RANGE = (200, 500)
+
+def init():
+     global image
+     global running
+     image = load_image('character_selection.png')
+     running = True
+
+def finish():
+ global image
+ del image
+
+def update():
+    show_character_selection()
+
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+def show_character_selection():
+    global characterjob
+    global running
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:  # 마우스 이벤트 처리
+            if event.button == SDL_BUTTON_LEFT:
+                # Y좌표 변환 (Pico2D 좌하단 (0,0) 기준)
+                mouse_x, mouse_y = event.x, 800 - 1 - event.y
+                # Y축 범위 내에 있는지 확인
+                if CHARACTER_Y_RANGE[0] <= mouse_y <= CHARACTER_Y_RANGE[1]:
+
+                    # 1. Swordsman 선택 영역 확인
+                    if CHARACTER_POSITIONS['Swordsman'][0] <= mouse_x <= CHARACTER_POSITIONS['Swordsman'][1]:
+                        characterjob = 'Swordsman'
+                        game_framework.change_mode(play_mode)
+
+                        clear_canvas()
+
+                    # 2. Wizard 선택 영역 확인
+                    elif CHARACTER_POSITIONS['Wizard'][0] <= mouse_x <= CHARACTER_POSITIONS['Wizard'][1]:
+                        characterjob = 'Wizard'
+                        game_framework.change_mode(play_mode)
+                        clear_canvas()
+
+                    # 3. Archer 선택 영역 확인
+                    elif CHARACTER_POSITIONS['Archer'][0] <= mouse_x <= CHARACTER_POSITIONS['Archer'][1]:
+                        characterjob = 'Archer'
+                        game_framework.change_mode(play_mode)
+                        clear_canvas()
---
+++ play_mode.py
@@ -0,0 +1,46 @@
+from pico2d import *
+import Skill
+import game_world
+from Item import *
+import game_framework
+from monster import Monster
+from Character import Character
+
+def handle_events():
+    global running
+    global skill_effect
+    global characters
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            characters.handle_event(event)
+
+def init():
+    global characters
+    global running
+
+    running = True
+    characters = Character(characterjob)
+    game_world.add_object(characters, 1)
+
+    monster = Monster(characters)
+    game_world.add_object(monster, 1)
+
+def update():
+    game_world.update()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
