6f6528b344c35d5527aff5aa08760251475182e2
Jo JangHyen(home) <pa7333@gmail.com>
2025-11-12 20:14:14 +0900

    아이템 공중 부유 애니메이션 구현

--- Item.py
+++ Item.py
@@ -1,11 +1,20 @@
 from pico2d import load_image
+import game_framework
 
-class item:
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 2.5  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+class Item:
     image = None
     def __init__(self, item_type):
         self.x = 600
         self.y = 300
         self.type = item_type
+        self.direction = 0.5
+        self.max_move = 0
         if self.type == 0:
             self.image = load_image('item/item1.png')
         elif self.type == 1:
@@ -17,7 +26,12 @@ class item:
         elif self.type == 4:
             self.image = load_image('item/item5.png')
     def update(self):
-        pass
+        self.y = self.y + RUN_SPEED_PPS * self.direction * game_framework.frame_time
+        self.max_move += RUN_SPEED_PPS * self.direction * game_framework.frame_time
+        if abs(self.max_move) >= 6.0:
+            self.direction = -self.direction
+            self.max_move = 0
+
 
     def draw(self):
         self.image.draw(self.x, self.y)
