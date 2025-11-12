fcf1be983138077adcb6686fc14f84b3cd24207d
Jo JangHyen(home) <pa7333@gmail.com>
2025-11-12 21:28:05 +0900

    기본 공격 투사체와 몬스터의 충돌 페어 추가

--- play_mode.py
+++ play_mode.py
@@ -33,6 +33,7 @@ def init(job_name):
     game_world.add_collision_pair('character:item', characters, None)
     game_world.add_collision_pair('character:monster', characters, monster)
     game_world.add_collision_pair('skill:monster', None, monster)
+    game_world.add_collision_pair('projectile:monster', None, monster)
 
 def update():
     game_world.update()
