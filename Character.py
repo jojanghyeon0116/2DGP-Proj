be72c65360eae3f89f93314c2798d8412d0b043c
Jo JangHyen(home) <pa7333@gmail.com>
2025-11-12 18:48:23 +0900

    스킬 생성시 몬스터와 충돌 페어 추가

--- Character.py
+++ Character.py
@@ -345,12 +345,15 @@ class Character:
     def Skill_1(self):
         skill1 = skill_1(self.x, self.y, self.direction_x, self.job)
         game_world.add_object(skill1, 1)
+        game_world.add_collision_pair('skill:monster', skill1, None)
     def Skill_2(self):
         skill2 = skill_2(self.x, self.y, self.direction_x, self.job)
         game_world.add_object(skill2, 1)
+        game_world.add_collision_pair('skill:monster', skill2, None)
     def Skill_3(self):
         skill3 = skill_3(self.x, self.y, self.direction_x, self.job)
         game_world.add_object(skill3, 1)
+        game_world.add_collision_pair('skill:monster', skill3, None)
 
     def handle_collision(self, group, other):
         if group == 'character:monster':
