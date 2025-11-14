404f2d29cadcdf90ffedb67d4a81aa00f1e48371
Jo JangHyen(notebook) <pa7333@gmail.com>
2025-11-14 16:11:02 +0900

    platform의 충돌 추가

--- background.py
+++ background.py
@@ -1,4 +1,6 @@
-from pico2d import load_image
+from pico2d import *
+
+
 
 class Ground:
     def __init__(self):
@@ -14,9 +16,18 @@ class Ground:
 class Platform:
     def __init__(self):
         self.image = load_image('background/background2.png')
+        self.x = 200
+        self.y = 170
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(1024 // 2, 200, 100, 100)
+        self.image.draw(self.x, self.y, 100, 100)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 40, self.y - 25, self.x + 40, self.y + 25
+
+    def handle_collision(self, group, other):
+        pass
