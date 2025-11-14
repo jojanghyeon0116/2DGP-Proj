b8977f01b4b9bfb06adf321a79353362e1c82ab5
Jo JangHyen(notebook) <pa7333@gmail.com>
2025-11-14 15:15:21 +0900

    공중 블럭 Platform의 class 뼈대 완성

--- background.py
+++ background.py
@@ -10,3 +10,13 @@ class Ground:
     def draw(self):
         self.image.draw(1024 // 2, 800 // 2)
 
+
+class Platform:
+    def __init__(self):
+        self.image = load_image('background/background2.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(1024 // 2, 200, 100, 100)
