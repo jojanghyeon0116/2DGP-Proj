7d8eab2e0ecf55608c796063c5e91fe134755074
Jo JangHyen(home) <pa7333@gmail.com>
2025-11-20 03:46:02 +0900

    스테이지 3 초기 설정

---
+++ stage_3.py
@@ -0,0 +1,52 @@
+from pico2d import *
+import Skill
+import game_world
+from Item import *
+import game_framework
+from Character import Character
+from background import Boss
+import monster
+
+def handle_events():
+    global running
+    global skill_effect
+    global characters
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            characters.handle_event(event)
+
+def init(job_name):
+    global characters
+    global running
+    global platform
+
+    running = True
+    characters = Character(job_name, 400, 200)
+    game_world.add_object(characters, 1)
+
+    boss = Boss(characters)
+    game_world.add_object(boss, 0)
+
+    game_world.add_collision_pair('character:item', characters, None)
+    game_world.add_collision_pair('character:back_ground', characters, boss)
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
