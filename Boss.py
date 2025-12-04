e641d08b6742fd1ba0637a167bf387f80ddab53f
Jo JangHyen(home) <pa7333@gmail.com>
2025-12-04 21:32:38 +0900

    초기 뼈대 완성

---
+++ Boss.py
@@ -0,0 +1,16 @@
+class Monster:
+    image = None
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
+
+    def handle_collision(self):
+        pass
