a9a3db784e0e6cb1dec28f24db080a7b71089568
Jo JangHyen(notebook) <pa7333@gmail.com>
2025-12-05 16:32:30 +0900

    뼈대 완성

---
+++ Shop_window.py
@@ -0,0 +1,28 @@
+from pico2d import *
+from Item import *
+import game_framework
+
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+
+def init():
+    pass
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
