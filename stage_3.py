234cb5efa5f0b9e6f0910fba39fa6f4bf3728328
Jo JangHyen(home) <pa7333@gmail.com>
2025-12-08 19:24:27 +0900

    common을 이용하도록 수정

--- stage_3.py
+++ stage_3.py
@@ -7,12 +7,11 @@ from Character import Character
 from background import Boss
 import monster
 import UI
-
+import common
 
 def handle_events():
     global running
     global skill_effect
-    global characters
     event_list = get_events()
     for event in event_list:
         if event.type == SDL_QUIT:
@@ -20,38 +19,37 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         else:
-            characters.handle_event(event)
+            common.character.handle_event(event)
 
 def init(job_name, current_hp=None, current_money=None, current_level=None):
-    global characters
     global running
     global platform
 
     running = True
-    characters = Character(job_name, 400, current_hp=current_hp, current_money=current_money)
-    game_world.add_object(characters, 1)
+    common.character = Character(job_name, 400, current_hp=current_hp, current_money=current_money)
+    game_world.add_object(common.character, 1)
 
-    boss = Boss(characters)
+    boss = Boss(common.character)
     game_world.add_object(boss, 0)
 
-    health_bar = UI.HealthBar(characters)
+    health_bar = UI.HealthBar(common.character)
     game_world.add_object(health_bar, 0)
 
-    skill_icon1 = UI.Skill_icon1(characters)
+    skill_icon1 = UI.Skill_icon1(common.character)
     game_world.add_object(skill_icon1, 0)
-    skill_icon2 = UI.Skill_icon2(characters)
+    skill_icon2 = UI.Skill_icon2(common.character)
     game_world.add_object(skill_icon2, 0)
-    skill_icon3 = UI.Skill_icon3(characters)
+    skill_icon3 = UI.Skill_icon3(common.character)
     game_world.add_object(skill_icon3, 0)
 
-    money_display = UI.MoneyDisplay(characters)
+    money_display = UI.MoneyDisplay(common.character)
     game_world.add_object(money_display, 0)
 
-    level = UI.Level(characters)
+    level = UI.Level(common.character)
     game_world.add_object(level, 0)
 
-    game_world.add_collision_pair('character:item', characters, None)
-    game_world.add_collision_pair('character:back_ground', characters, boss)
+    game_world.add_collision_pair('character:item', common.character, None)
+    game_world.add_collision_pair('character:back_ground', common.character, boss)
 
 def update():
     game_world.update()
