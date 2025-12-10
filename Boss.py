66e4b3f1cf6e92fa18ff895a4deed9a2fca2e316
Jo JangHyen(notebook) <pa7333@gmail.com>
2025-12-10 13:40:08 +0900

    보스 캐릭터 공격 추가

--- Boss.py
+++ Boss.py
@@ -27,30 +27,40 @@ class Boss:
         self.direction = 0
     def update(self):
         distance = self.x - common.character.x
-        if abs(distance) < 100:
-            self.type = 1
+        if abs(distance) < 200:
+            if abs(distance) < 100:
+                self.type = 2
+            else:
+                self.type = 1
+                self.x += self.direction * RUN_SPEED_PPS * game_framework.frame_time
             if distance < 0:
                 self.direction = 1
             elif distance > 0:
                 self.direction = -1
-            self.x += self.direction * RUN_SPEED_PPS * game_framework.frame_time
         else:
             self.type = 0
 
         if self.type == 0:
             self.frame_x = (self.frame_x + self.max_frame * ACTION_PER_TIME * game_framework.frame_time) % 5
             self.frame_y = 640
-        if self.type == 1:
+        elif self.type == 1:
             self.frame_x = (self.frame_x + self.max_frame * ACTION_PER_TIME * game_framework.frame_time) % 9
             self.frame_y = 480
+        elif self.type == 2:
+            self.frame_x = (self.frame_x + self.max_frame * ACTION_PER_TIME * game_framework.frame_time) % 15
+            self.frame_y = 320
+        elif self.type == 3:
+            self.frame_x = (self.frame_x + self.max_frame * ACTION_PER_TIME * game_framework.frame_time) % 5
     def draw(self):
         if self.direction == 0 or self.direction == -1:
             self.image.clip_draw(int(self.frame_x) * 288, self.frame_y, 288, 160, self.x, self.y, 500, 500)
         else:
             self.image.clip_composite_draw(int(self.frame_x) * 288, self.frame_y, 288, 160, 0,'h',self.x, self.y, 500, 500)
+        draw_rectangle(*self.get_bb())
     def get_bb(self):
-        pass
+        return self.x - 50, self.y - 250, self.x + 50, self.y
 
-    def handle_collision(self):
-        pass
+    def handle_collision(self, group, other):
+        if group == 'character:boss':
+            pass
 
