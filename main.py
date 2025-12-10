9b8d2d0b93a79454f0f2ad31c48ad4a80fd42bad
Jo JangHyen(notebook) <pa7333@gmail.com>
2025-12-10 20:53:23 +0900

    배경 음악 추가

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import game_framework
 import character_selection as start_mode
 
 open_canvas(1024, 800)
+bgm = load_music('InTheForest.wav')
+bgm.set_volume(100)
+bgm.repeat_play()
 game_framework.run(start_mode)
 update_canvas()
 
